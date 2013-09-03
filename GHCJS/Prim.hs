a8daf16ca0293cb6cfd0bbd0b8149a2806360139
Luite Stegeman <stegeman@gmail.com>
2013-09-04 00:30:57 +0200

    add wrapper for JS exceptions

--- GHCJS/Prim.hs
+++ GHCJS/Prim.hs
@@ -1,12 +1,37 @@
-{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE MagicHash, DeriveDataTypeable, JavaScriptFFI #-}
 
-module GHCJS.Prim (JSRef(..)) where
+module GHCJS.Prim (JSRef(..), JSException(..), mkJSException) where
 
-import GHC.Prim
+import           Data.Typeable (Typeable)
+import           Unsafe.Coerce (unsafeCoerce)
+
+import           GHC.Prim
+import qualified GHC.Exception as Ex
 
 {-
   JSRef is a boxed type that can be used as FFI
   argument or result.
 -}
 data JSRef a = JSRef ByteArray#
+
+{-
+  When a JavaScript exception is raised inside
+  a safe or interruptible foreign call, it is converted
+  to a JSException
+ -}
+data JSException = JSException (JSRef ()) String
+  deriving (Typeable)
+
+instance Ex.Exception JSException
+
+instance Show JSException where
+  show (JSException _ xs) = "JavaScript exception: " ++ xs
+
+foreign import javascript unsafe "h$toHsString(\"\" + $1)"
+   js_toString :: JSRef a -> IO Int
+
+mkJSException :: JSRef a -> IO JSException
+mkJSException ref = do
+    xs <- js_toString ref
+    return (JSException (unsafeCoerce ref) (unsafeCoerce xs))
 
